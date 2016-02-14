913c3dd86d2f27a4742641df01e8bcd20acf6732
Dillon McCoy <dillon@tellapart.com>
2016-02-14 14:04:31 -0800

    Added sharing

--- app/src/main/java/com/dillonmccoy/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/dillonmccoy/nytimessearch/activities/ArticleActivity.java
@@ -1,10 +1,16 @@
 package com.dillonmccoy.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,6 +22,7 @@ import org.parceler.Parcels;
 
 public class ArticleActivity extends AppCompatActivity {
     private WebView wvArticle;
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,27 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         wvArticle.loadUrl(article.getWebUrl());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_article, menu);
+
+        MenuItem item = menu.findItem(R.id.miShare);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        Log.e("ASDF" , "setting up sharing");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShareAction.setShareIntent(shareIntent);
+
+
+        return true;
     }
 
 }
